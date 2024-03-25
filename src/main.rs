96df37448cc8a78d9211bdc4993c342a2f532902
saksham <saksham@au-zone.com>
2024-03-25 15:38:48 -0600

    Added the radarcube thread

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,34 @@ use async_std::task::block_on;
 use cdr::{CdrLe, Infinite};
 use clap::Parser;
 use deepviewrt::context::Context;
-use drvegrd::Frame;
-use drvegrd::{load_data, read_frame, Packet};
+use drvegrd::{
+    eth::{RadarCube, RadarCubeReader},
+    load_data, read_frame, Frame, Packet,
+};
 use env_logger;
 use futures::join;
-use log::trace;
+use log::{error, trace};
+use ndarray::{concatenate, s, Array, Array5, Axis};
 use socketcan::{async_std::CanSocket, CanFrame, EmbeddedFrame, Id as CanId};
-use std::error::Error;
-use std::f32::consts::PI;
-use std::str::FromStr as _;
-use std::time::Instant;
-use std::time::{Duration, SystemTime};
+use std::{
+    error::Error,
+    f32::consts::PI,
+    net::UdpSocket,
+    str::FromStr as _,
+    sync::Arc,
+    thread,
+    time::{Duration, Instant, SystemTime},
+};
 use unix_ts::Timestamp;
 use zenoh::{config::Config, prelude::r#async::*};
-use zenoh_ros_type::common_interfaces::sensor_msgs::PointCloud2;
-use zenoh_ros_type::common_interfaces::sensor_msgs::PointField;
-use zenoh_ros_type::rcl_interfaces::builtin_interfaces::Time as ROSTime;
+use zenoh_ros_type::{
+    common_interfaces::sensor_msgs::{PointCloud2, PointField, RadCube},
+    rcl_interfaces::builtin_interfaces::Time as ROSTime,
+};
+
+#[cfg(feature = "can")]
+pub mod can;
+
 #[derive(Debug)]
 #[allow(dead_code)]
 pub enum PointFieldType {
@@ -31,7 +43,7 @@ pub enum PointFieldType {
     FLOAT64 = 8,
 }
 
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, Clone)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// zenoh connection mode
@@ -45,6 +57,10 @@ struct Args {
     /// ros topic
     #[arg(short, long, default_value = "rt/radar/targets")]
     topic: String,
+
+    /// ros topic
+    #[arg(short, long, default_value = "rt/radCube/radarcube")]
+    cube_topic: String,
 
     /// clustering by defaut its off
     #[arg(long)]
@@ -121,6 +137,113 @@ fn cluster_points(points: &Vec<Vec<f32>>, distance_threshold: f32) -> Vec<Vec<Ve
     clusters
 }
 
+fn format_cube(cube: RadarCube) -> Result<Array5<i16>, Box<dyn std::error::Error>> {
+    let ndcube = Array::from_shape_vec(cube.shape, cube.cube)?;
+    let orig_shape = ndcube.shape();
+
+    // Move the doppler zero point to the center of the cube.
+    let (right, left) = ndcube.view().split_at(Axis(3), 64);
+    let mut ndcube = concatenate(Axis(1), &[left, right])?;
+
+    // Invert the range axis so that the minimum is at the bottom.
+    ndcube.invert_axis(Axis(1));
+
+    trace!("format_cube shape {:?} -> {:?}", orig_shape, cube.shape);
+
+    Ok(ndcube)
+}
+
+fn udp_loop(session: Arc<Session>, topic: &String) -> Result<(), Box<dyn std::error::Error>> {
+    let (tx5, mut rx) = tachyonix::channel(100);
+    let tx63 = tx5.clone();
+
+    thread::spawn(move || {
+        let sock = UdpSocket::bind("0.0.0.0:50005").unwrap();
+        let mut buf = [0; 1500];
+
+        loop {
+            match sock.recv_from(&mut buf) {
+                Ok((n, _)) => match block_on(tx5.send(buf[..n].to_vec())) {
+                    Ok(_) => (),
+                    Err(e) => error!("port5 error: {:?}", e),
+                },
+                Err(e) => error!("port5 error: {:?}", e),
+            }
+        }
+    });
+
+    thread::spawn(move || {
+        let sock = UdpSocket::bind("0.0.0.0:50063").unwrap();
+        let mut buf = [0; 1500];
+
+        loop {
+            match sock.recv_from(&mut buf) {
+                Ok((n, _)) => match block_on(tx63.send(buf[..n].to_vec())) {
+                    Ok(_) => (),
+                    Err(e) => error!("port63 error: {:?}", e),
+                },
+                Err(e) => error!("port63 error: {:?}", e),
+            }
+        }
+    });
+
+    let mut reader = RadarCubeReader::default();
+
+    loop {
+        let msg = match block_on(rx.recv()) {
+            Ok(msg) => msg,
+            Err(e) => {
+                error!("recv error: {:?}", e);
+                continue;
+            }
+        };
+        match reader.read(&msg) {
+            Ok(Some(cubemsg)) => {
+                let cube = format_cube(cubemsg.clone())?;
+                let seq_data = cube.slice(s![0, .., .., .., ..]).to_owned();
+                let seq_bytes = seq_data.iter().flat_map(|&x| x.to_le_bytes()).collect();
+
+                let ts: Timestamp = SystemTime::now()
+                    .duration_since(SystemTime::UNIX_EPOCH)?
+                    .into();
+                let shape: Vec<u16> = cube.shape().iter().map(|&x| x as u16).collect();
+
+                let msg = RadCube {
+                    header: zenoh_ros_type::std_msgs::Header {
+                        stamp: ROSTime {
+                            sec: ts.seconds() as i32,
+                            nanosec: ts.subsec(9),
+                        },
+                        frame_id: "".to_string(),
+                    },
+                    layout: seq_bytes,
+                    shape,
+                    scales: vec![
+                        cubemsg.bin_properties.speed_per_bin,
+                        cubemsg.bin_properties.range_per_bin,
+                        cubemsg.bin_properties.bin_per_speed,
+                    ],
+                    cube: cubemsg.clone().cube,
+                    is_complex: true,
+                };
+
+                let encoded = cdr::serialize::<_, _, CdrLe>(&msg, Infinite)?;
+                let _ = session
+                    .put(topic, encoded.clone())
+                    .encoding(Encoding::WithSuffix(
+                        KnownEncoding::AppOctetStream,
+                        "sensor_msgs/msg/RadCube".into(),
+                    ))
+                    .res_async();
+
+                trace!("Sending RadarCube message");
+            }
+            Ok(None) => (),
+            Err(err) => error!("Cube Error: {:?}", err),
+        }
+    }
+}
+
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
@@ -132,7 +255,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
     config.set_mode(Some(mode)).unwrap();
     config.connect.endpoints = args.endpoint.iter().map(|v| v.parse().unwrap()).collect();
     let _ = config.scouting.multicast.set_enabled(Some(false));
-    let session = zenoh::open(config.clone()).res_async().await.unwrap();
+    let session = zenoh::open(config.clone())
+        .res_async()
+        .await
+        .unwrap()
+        .into_arc();
     log::info!("Opened Zenoh session");
 
     let mut classifier = match &args.model {
@@ -148,6 +275,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let sock = CanSocket::open(&args.can)?;
     let mut windowed_objs = Vec::new();
     let mut head = 0;
+    let session_clone = session.clone();
+    let cube_topic = args.clone().cube_topic;
+    thread::spawn(move || {
+        let _ = udp_loop(session_clone, &cube_topic);
+    });
 
     loop {
         match read_frame(read_packet, &sock) {
@@ -424,11 +556,7 @@ fn transform_xyz(range: f32, azimuth: f32, elevation: f32, mirror: bool) -> [f32
     let x = range * ele.cos() * azi.cos();
     let y = range * ele.cos() * azi.sin();
     let z = range * ele.sin();
-    if mirror {
-        [x, -y, z]
-    } else {
-        [x, y, z]
-    }
+    if mirror { [x, -y, z] } else { [x, y, z] }
 }
 
 async fn send_timing(
